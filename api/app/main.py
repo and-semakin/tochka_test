5a555dd9d3f9c91d6b2977aae5d3280333e2a03e
Andrey Semakin <and-semakin@ya.ru>
2019-09-04 13:11:19 +0500

    Generate template app with `adev start` command

---
+++ api/app/main.py
@@ -0,0 +1,59 @@
+import asyncpg
+from aiohttp import web
+
+from .db import prepare_database
+from .settings import Settings
+
+
+async def startup(app: web.Application):
+    settings: Settings = app['settings']
+    await prepare_database(settings, False)
+    app['pg'] = await asyncpg.create_pool(dsn=settings.pg_dsn, min_size=2)
+
+
+async def cleanup(app: web.Application):
+    await app['pg'].close()
+
+
+async def ping(*args):
+    return web.json_response({
+        'status': 'I am still alive!',
+    }, status=200)
+
+
+async def kill():
+    # TODO: implement
+    pass
+
+
+async def add():
+    # TODO: implement
+    pass
+
+
+async def subtract():
+    # TODO: implement
+    pass
+
+
+async def status():
+    # TODO: implement
+    pass
+
+
+async def create_app():
+    app = web.Application()
+    settings = Settings()
+    app.update(
+        settings=settings,
+    )
+
+    app.on_startup.append(startup)
+    app.on_cleanup.append(cleanup)
+
+    app.router.add_get('/api/ping', ping, name='ping')
+    app.router.add_get('/api/kill', kill, name='kill')
+    app.router.add_post('/api/add', add, name='add')
+    app.router.add_post('/api/subtract', subtract, name='subtract')
+    app.router.add_get('/api/status', status, name='status')
+    return app
