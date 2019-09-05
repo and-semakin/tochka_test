9947ee8f96af6e8d7594f688896f315adfc2892d
Andrey Semakin <and-semakin@ya.ru>
2019-09-05 22:26:48 +0500

    Setup pydantic

--- api/app/settings.py
+++ api/app/settings.py
@@ -1,5 +1,3 @@
-from urllib.parse import urlparse
-
 from pydantic import BaseSettings
 
 
@@ -7,13 +5,15 @@ class Settings(BaseSettings):
     """
     See https://pydantic-docs.helpmanual.io/#settings for details on using and overriding this
     """
-    name = 'api'
-    pg_dsn = 'postgres://bank:bank@postgres:5432/bank'
+    postgres_host = 'localhost'
+    postgres_port = 5432
+    postgres_user = 'user'
+    postgres_password = 'secret'
+    postgres_db = 'db'
 
     @property
-    def _pg_dsn_parsed(self):
-        return urlparse(self.pg_dsn)
-
-    @property
-    def pg_name(self):
-        return self._pg_dsn_parsed.path.lstrip('/')
+    def pg_dsn(self) -> str:
+        return (
+            f'postgres://{self.postgres_user}:{self.postgres_password}@'
+            f'{self.postgres_host}:{self.postgres_port}/{self.postgres_db}'
+        )
