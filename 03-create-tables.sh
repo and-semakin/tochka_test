9f990491aa2f3b507bed3f2e634c8ac5cf702919
Andrey Semakin <and-semakin@ya.ru>
2019-09-05 22:43:52 +0500

    Add SQL query to init database

--- 03-create-tables.sh
+++ 03-create-tables.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 command -v docker-compose >/dev/null 2>&1 || { echo >&2 "This script requires `docker-compose` but it's not installed.  Aborting."; exit 1; }
-docker-compose ps
+QUERIES=./sql/*.sql
+for q in $QUERIES
+do
+  echo "Processing $q..."
+  docker-compose exec -T postgres psql -U bank < $q
+  echo "====================="
+done
+echo "Done."
